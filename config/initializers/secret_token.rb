a57dedff8d31e4c3ed94a27fe2bb43b3077aeea6
siac web <siac.bar.web@gmail.com>
2013-05-31 16:44:31 -0300

    Crição do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siacweb::Application.config.secret_token = '2dec3a123f5b5844c8da2b0ddf26812c30c20dafc1530733d938d6659cacb66b6e0386444d18ee58048f2095ed105bf3e2dec1261760f137703de246ea605a85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Siacweb::Application.config.session_store :cookie_store, key: '_siacweb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Siacweb::Application.config.session_store :active_record_store
