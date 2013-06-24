8be58f33d5f715ca8c24ad9b27537054579d4478
siac web <siac.bar.web@gmail.com>
2013-06-23 22:24:30 -0300

    criação novos campos - home

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,7 +1,7 @@
 class Home < ActiveRecord::Base
-  attr_accessible :title, :main_image, :gallery_image, :events_image
+  attr_accessible :title, :description, :main_image, :gallery_image, :events_image, :contact_image, :follow_fb_image, :follow_tw_image
 
-  validates_attachment_presence :main_image, :gallery_image, :events_image
+  validates_attachment_presence :main_image, :gallery_image, :events_image, :contact_image
 
   has_attached_file :main_image,
                     :styles => {:medium => "400x300#", :thumb => "100x100#"},
@@ -15,5 +15,17 @@ class Home < ActiveRecord::Base
                     :styles => {:medium => "400x300#", :thumb => "100x100#"},
                     :url => "/assets/homes/:id/images/events_:style.:extension",
                     :path => ":rails_root/public/assets/homes/:id/images/events_:style.:extension"
+  has_attached_file :contact_image,
+                    :styles => {:medium => "400x300#", :thumb => "100x100#"},
+                    :url => "/assets/homes/:id/images/contact_:style.:extension",
+                    :path => ":rails_root/public/assets/homes/:id/images/contact_:style.:extension"
+  has_attached_file :follow_fb_image,
+                    :styles => {:medium => "400x300#", :thumb => "100x45#"},
+                    :url => "/assets/homes/:id/images/follow_fb_:style.:extension",
+                    :path => ":rails_root/public/assets/homes/:id/images/follow_fb_:style.:extension"
+  has_attached_file :follow_tw_image,
+                    :styles => {:medium => "400x300#", :thumb => "100x45#"},
+                    :url => "/assets/homes/:id/images/follow_tw_:style.:extension",
+                    :path => ":rails_root/public/assets/homes/:id/images/follow_tw_:style.:extension"
 
 end
--- db/migrate/20130619120147_create_homes.rb
+++ db/migrate/20130619120147_create_homes.rb
@@ -2,6 +2,8 @@ class CreateHomes < ActiveRecord::Migration
   def up
     create_table :homes do |t|
       t.string :title
+      t.string :description
+
       t.string :main_image_file_name
       t.string :main_image_content_type
       t.integer :main_image_file_size
@@ -16,6 +18,21 @@ class CreateHomes < ActiveRecord::Migration
       t.string :events_image_content_type
       t.integer :events_image_file_size
       t.datetime :events_image_updated_at
+
+      t.string :contact_image_file_name
+      t.string :contact_image_content_type
+      t.integer :contact_image_file_size
+      t.datetime :contact_image_updated_at
+
+      t.string :follow_fb_image_file_name
+      t.string :follow_fb_image_content_type
+      t.integer :follow_fb_image_file_size
+      t.datetime :follow_fb_image_updated_at
+
+      t.string :follow_tw_image_file_name
+      t.string :follow_tw_image_content_type
+      t.integer :follow_tw_image_file_size
+      t.datetime :follow_tw_image_updated_at
 
       t.timestamps
     end
